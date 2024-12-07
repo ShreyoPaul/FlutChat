18513e16443b54dbdb8d20b735d449ac5221dd4f
unknown <shreyopaul403@gmail.com>
2024-12-07 10:45:58 +0530

    const keyword added where required!

--- lib/components/chatBubble.dart
+++ lib/components/chatBubble.dart
@@ -4,19 +4,20 @@ class ChatBubble extends StatelessWidget {
   final String text;
   final bool isCurrentUser;
 
-  ChatBubble({super.key, required this.text, required this.isCurrentUser});
+  const ChatBubble(
+      {super.key, required this.text, required this.isCurrentUser});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      padding: EdgeInsets.all(10),
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
           color: isCurrentUser ? Colors.green : Colors.grey.shade800,
           borderRadius: BorderRadius.circular(10)),
       child: Text(
         text,
-        style: TextStyle(color: Colors.white),
+        style: const TextStyle(color: Colors.white),
       ),
     );
   }
